d40228800a2131d8af343c5380174296c7e95417
Michael Wettstein <chischte@gmx.ch>
2020-05-26 19:53:46 +0200

    Set up bootstrap

--- todoApp/src/app/app.module.ts
+++ todoApp/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './components/app/app.component';
+import {LoginComponent} from './components/login/login.component';
 import {FormsModule} from "@angular/forms";
-import { ListTodosComponent } from './components/list-todos/list-todos.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { ErrorComponent } from './components/error/error.component';
+import {ListTodosComponent} from './components/list-todos/list-todos.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {ErrorComponent} from './components/error/error.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -17,12 +18,14 @@ import { ErrorComponent } from './components/error/error.component';
     WelcomeComponent,
     ErrorComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
